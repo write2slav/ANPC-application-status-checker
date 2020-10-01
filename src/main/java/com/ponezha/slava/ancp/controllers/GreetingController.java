c797ad26f7a907f85021b038dfd555351ebd9503
Slava <write2slava@gmail.com>
2020-10-01 18:55:57 +0100

    Text translated into russian

--- src/main/java/com/ponezha/slava/ancp/controllers/GreetingController.java
+++ src/main/java/com/ponezha/slava/ancp/controllers/GreetingController.java
@@ -26,14 +26,14 @@ public class GreetingController {
     public String search(@RequestParam(name = "caseNumber", required = true, defaultValue = "") String name, Model model) {
 
         if (name.length() < 6) {
-            model.addAttribute("message", "Please type at least six characters of your case number");
+            model.addAttribute("message", "Пожалуйста, введите минимум 6 цифр номера досара в указанном в примере формате");
             return "search";
         }
         if (ordinsRepo.findByCaseNumber(name).size() == 0) {
-            model.addAttribute("message", ":( Nothing found... Please try again later.");
+            model.addAttribute("message", "Ваш номер досара не найден в базе данных приказов. Попробуйте позже. Мы обновляем базу данных каждый день.");
             return "search";
         } else {
-            model.addAttribute("message", "You case was processed :) . You can find you case in the Ordin following this link:");
+            model.addAttribute("message", "Ваш досар найден в приказе:");
             model.addAttribute("number", name);
             model.addAttribute("caseNumber", ordinsRepo.findByCaseNumber(name).get(0));
             return "search";
